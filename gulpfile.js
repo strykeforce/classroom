df268bce87a00d8954bd5942c05a20dd72a772ff
Jeff Hutchison <jeff@jeffhutchison.com>
2021-04-03 08:41:04 -0400

    Add source watch capability

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
-const { src, dest, parallel, series } = require("gulp");
+const { src, dest, parallel, series, lastRun, watch } = require("gulp");
 const execa = require("execa");
+const log = require("fancy-log");
 
 const THEME = "moon";
 const HIGHLIGHT = "zenburn";
+const LEVEL = 2;
 
-const PRESENTATIONS = [
+const SLIDES = [
   {
-    slug: "paths",
-    files: ["src/paths/index.md"],
+    slug: "markdown",
+    files: ["index.md", "colophone.md"],
+  },
+  {
+    slug: "swerve",
+    files: ["index.md"],
   },
 ];
 
@@ -16,38 +22,78 @@ const REVEALJS_SRC = "node_modules/reveal.js";
 const REVEALJS_DEST = "build/static/reveal.js";
 
 const copyRevealJSDist = () =>
-  src([
-    `${REVEALJS_SRC}/dist/**/*`,
-    `!${REVEALJS_SRC}/dist/**/*.esm.js`,
-    `!${REVEALJS_SRC}/dist/**/*.map`,
-  ]).pipe(dest(`${REVEALJS_DEST}/dist`));
+  src(
+    [
+      `${REVEALJS_SRC}/dist/**/*`,
+      `!${REVEALJS_SRC}/dist/*.esm.js`,
+      `!${REVEALJS_SRC}/dist/*.map`,
+    ],
+    { since: lastRun(copyRevealJSDist) }
+  ).pipe(dest(`${REVEALJS_DEST}/dist`));
 
 const copyRevealJSPlugins = () =>
-  src([
-    `${REVEALJS_SRC}/plugin/**`,
-    `!${REVEALJS_SRC}/plugin/highlight/**`,
-    `!${REVEALJS_SRC}/plugin/markdown/**`,
-    `!${REVEALJS_SRC}/plugin/math/**`,
-  ]).pipe(dest(`${REVEALJS_DEST}/plugin`));
+  src(
+    [
+      `${REVEALJS_SRC}/plugin/**`,
+      `!${REVEALJS_SRC}/plugin/highlight/**`,
+      `!${REVEALJS_SRC}/plugin/markdown/**`,
+      `!${REVEALJS_SRC}/plugin/math/**`,
+    ],
+    { since: lastRun(copyRevealJSDist) }
+  ).pipe(dest(`${REVEALJS_DEST}/plugin`));
 
-const buildRevealJS = async (slug, slides) => {
-  await fs.promises.mkdir(`build/${slug}`, { recursive: true });
+const createDestDir = async (slug) => {
+  const destDir = `build/${slug}`;
+  log(`creating ${destDir} directory`);
+  await fs.promises.mkdir(destDir, { recursive: true });
+};
+
+const buildSlides = async (slug, files) => {
+  const srcDir = `src/${slug}`;
+  const destDir = `build/${slug}`;
+
+  log(`building slides in ${srcDir}`);
 
   const args = [
     "--to=revealjs",
     "--standalone",
     "--variable=revealjs-url:../static/reveal.js",
+    `--slide-level=${LEVEL}`,
     `--variable=theme:${THEME}`,
     `--highlight-style=${HIGHLIGHT}`,
     `--output=build/${slug}/index.html`,
   ];
 
-  await execa("pandoc", args.concat(slides), { stdio: "inherit" });
+  await execa("pandoc", args.concat(files.map((md) => `${srcDir}/${md}`)), {
+    stdio: "inherit",
+  });
+};
+
+const copyImages = (slug) => {
+  const srcDir = `src/${slug}`;
+  const destDir = `build/${slug}`;
+
+  log(`copying images to ${destDir}`);
+
+  return src(`${srcDir}/img/*`).pipe(dest(`${destDir}/img`));
 };
 
 const revealJS = parallel(copyRevealJSDist, copyRevealJSPlugins);
 
-exports.default = parallel(
+exports.buildAll = parallel(
   revealJS,
-  PRESENTATIONS.map(({ slug, files }) => () => buildRevealJS(slug, files))
+  SLIDES.map(({ slug, files }) =>
+    series(
+      () => createDestDir(slug),
+      parallel(
+        () => buildSlides(slug, files),
+        () => copyImages(slug)
+      )
+    )
+  )
 );
+
+exports.watch = () =>
+  watch("src/*/*.md", { ignoreInitial: false }, this.buildAll);
+
+exports.default = this.buildAll;
